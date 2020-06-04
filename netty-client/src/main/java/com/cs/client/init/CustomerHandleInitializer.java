abd66760782e769f1a9a7f88337fb4b6c71bee08
pengfei.wang <Cc123456>
2020-06-04 14:19:14 +0800

    更新描述

--- netty-client/src/main/java/com/cs/client/init/CustomerHandleInitializer.java
+++ netty-client/src/main/java/com/cs/client/init/CustomerHandleInitializer.java
@@ -8,13 +8,11 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.handler.timeout.IdleStateHandler;
 
 /**
- * Created by haoxy on 2018/10/17.
- * E-mail:hxyHelloWorld@163.com
- * github:https://github.com/haoxiaoyong1014
+ *自定义protobuf解码器
  */
 public class CustomerHandleInitializer extends ChannelInitializer<Channel> {
     @Override
-    protected void initChannel(Channel channel) throws Exception {
+    protected void initChannel(Channel channel) {
         channel.pipeline()
                 //10 秒没发送消息 将IdleStateHandler 添加到 ChannelPipeline 中
                 // .addLast(new IdleStateHandler(0, 10, 0))
